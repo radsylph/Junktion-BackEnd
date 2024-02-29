f49230d78e57a56c29b2912857dcd0f5e19f4873
radsylph <andresa@devtop.io>
2024-02-29 13:36:11 -0400

    created publication.interface.ts

---
+++ src/interfaces/publication.interface.ts
@@ -0,0 +1,10 @@
+export interface PublicationInterface {
+    title: string;
+    content: string;
+    images: [string];
+    owner: string;
+    isEdited: boolean;
+    bookMark: boolean;
+    likes: number;
+    comments: number;
+}
