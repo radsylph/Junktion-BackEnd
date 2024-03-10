5a7c2055be4e65a991c671ca91eac7f58f487a2f
radsylph <andresa@devtop.io>
2024-03-10 13:55:00 -0400

    chore:added new routes

--- src/routes/publications.routes.ts
+++ src/routes/publications.routes.ts
@@ -6,7 +6,13 @@ import {
     editPublication,
     getUserPublications,
     getLikes,
-    getLikesPublication
+    getLikesPublication,
+    bookMarkPublication,
+    getBookMarks,
+    getBookMarksPublication,
+    getMyBookMarks,
+    deletePublication,
+
 } from "../controllers/publications.controllers";
 import passport from "passport";
 
@@ -18,10 +24,15 @@ router.post(
     passport.authenticate("jwt", { session: false }),
     createPublication
 );
+router.delete("/delete/:publicationId", passport.authenticate("jwt", { session: false }), deletePublication);
 router.patch("/like/:publicationId", passport.authenticate("jwt", { session: false }), likePublication)
 router.put("/edit/:publicationId", passport.authenticate("jwt", { session: false }), editPublication)
 router.get("/get/:userId", passport.authenticate("jwt", { session: false }), getUserPublications)
 router.get("/like/:publicationId", passport.authenticate("jwt", { session: false }), getLikesPublication)
-router.get("/like", passport.authenticate("jwt", { session: false }), getLikes)
+router.get("/like", passport.authenticate("jwt", { session: false }), getLikes),
+    router.patch("/bookmark/:postId", passport.authenticate("jwt", { session: false }), bookMarkPublication)
+router.get("/bookmark", passport.authenticate("jwt", { session: false }), getBookMarks)
+router.get("/bookmark/:postId", passport.authenticate("jwt", { session: false }), getBookMarksPublication)
+router.get("/:userId/bookmark", passport.authenticate("jwt", { session: false }), getMyBookMarks);
 
 export default router;
