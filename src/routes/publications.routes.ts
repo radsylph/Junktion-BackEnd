565ab30599d536586b9c57ac55fa69641923fbd2
radsylph <andresa@devtop.io>
2024-03-11 15:25:08 -0400

    Chore:added new routes

--- src/routes/publications.routes.ts
+++ src/routes/publications.routes.ts
@@ -12,6 +12,15 @@ import {
     getBookMarksPublication,
     getMyBookMarks,
     deletePublication,
+    sendFriendRequest,
+    acceptFriendRequest,
+    rejectFriendRequest,
+    getFriendsRequest,
+    getFriendsPublications,
+    getPublication,
+    getFriends,
+    deleteFriend,
+    getMyFriends,
 
 } from "../controllers/publications.controllers";
 import passport from "passport";
@@ -19,6 +28,7 @@ import passport from "passport";
 const router = express.Router();
 
 router.get("/get", passport.authenticate("jwt", { session: false }), getPublications);
+router.get("/get/:publicationId", passport.authenticate("jwt", { session: false }), getPublication);
 router.post(
     "/create",
     passport.authenticate("jwt", { session: false }),
@@ -27,12 +37,21 @@ router.post(
 router.delete("/delete/:publicationId", passport.authenticate("jwt", { session: false }), deletePublication);
 router.patch("/like/:publicationId", passport.authenticate("jwt", { session: false }), likePublication)
 router.put("/edit/:publicationId", passport.authenticate("jwt", { session: false }), editPublication)
-router.get("/get/:userId", passport.authenticate("jwt", { session: false }), getUserPublications)
+router.get("/getAll/:userId", passport.authenticate("jwt", { session: false }), getUserPublications)
 router.get("/like/:publicationId", passport.authenticate("jwt", { session: false }), getLikesPublication)
 router.get("/like", passport.authenticate("jwt", { session: false }), getLikes),
     router.patch("/bookmark/:postId", passport.authenticate("jwt", { session: false }), bookMarkPublication)
 router.get("/bookmark", passport.authenticate("jwt", { session: false }), getBookMarks)
 router.get("/bookmark/:postId", passport.authenticate("jwt", { session: false }), getBookMarksPublication)
 router.get("/:userId/bookmark", passport.authenticate("jwt", { session: false }), getMyBookMarks);
+router.get("/getFriendRequests", passport.authenticate("jwt", { session: false }), getFriendsRequest);
+router.post("/sendFriendRequest/:receiver", passport.authenticate("jwt", { session: false }), sendFriendRequest);
+router.post("/acceptFriendRequest/:sender", passport.authenticate("jwt", { session: false }), acceptFriendRequest);
+router.post("/rejectFriendRequest/:sender", passport.authenticate("jwt", { session: false }), rejectFriendRequest);
+//router.get("/getFriendRequests", passport.authenticate("jwt", { session: false }), getFriendsRequest);
+router.get("/getFriendsPublications", passport.authenticate("jwt", { session: false }), getFriendsPublications);
+router.get("/getFriends/:userId", passport.authenticate("jwt", { session: false }), getFriends);
+router.delete("/deleteFriend/:friendId", passport.authenticate("jwt", { session: false }), deleteFriend);
+
 
 export default router;
