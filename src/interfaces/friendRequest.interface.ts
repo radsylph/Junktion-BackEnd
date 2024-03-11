323e2d21df4d000bed688e3107113ac96210669a
radsylph <andresa@devtop.io>
2024-03-11 15:23:27 -0400

    created friendRequest.interface.ts

---
+++ src/interfaces/friendRequest.interface.ts
@@ -0,0 +1,10 @@
+export interface FriendInterface {
+    mySelf: string;
+    myFriend: string;
+}
+
+export interface FriendRequestInterface {
+    sender: string;
+    receiver: string;
+    status: string;
+}
