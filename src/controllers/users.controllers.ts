60cf7b789563aee2073fbf80876f53290784fc5d
radsylph <andresa@devtop.io>
2024-03-13 02:08:09 -0400

    Chore:added controllers and routes to handle user delete

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import passport from "../configs/passport";
 import User from "../models/user";
+import Friend from "../models/friend";
+import FriendRequest from "../models/friendRequest";
+import Publication from "../models/publication";
+import Like from "../models/like";
 import AuthRequest from "../interfaces/authRequest.interface";
 import { generateToken, decryptToken } from "../utils/jwt";
+import BookMark from "../models/bookMark";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     await passport.authenticate("signup", (err: any, user: any, info: any) => {
@@ -54,6 +59,29 @@ const deleteUser = async (req: Request, res: Response) => {
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
+        const userLikes = await Like.find({ userId: user._id }).exec();
+        await Promise.all(userLikes.map(async (like) => {
+            await Publication.findByIdAndUpdate(like.publicationId, {
+                $inc: { likes: -1 },
+            }).exec();
+            await Like.findByIdAndDelete(like._id).exec();
+        }));
+        const userFriends = await Friend.find({ mySelf: user._id }).exec();
+        await Promise.all(userFriends.map(async (friend) => {
+            await User.findByIdAndUpdate(friend.myFriend, {
+                $inc: { friends: -1 },
+            }).exec();
+            const friends1 = await Friend.find({ mySelf: user._id, myFriend: friend.myFriend }).exec();
+            await Promise.all(friends1.map((friend) => Friend.findByIdAndDelete(friend._id).exec()));
+            const friends2 = await Friend.find({ mySelf: friend.myFriend, myFriend: user._id }).exec();
+            await Promise.all(friends2.map((friend) => Friend.findByIdAndDelete(friend._id).exec()));
+        }));
+        const userPublications = await Publication.find({ owner: user._id }).exec();
+        await Promise.all(userPublications.map(async (publication) => {
+            await Like.deleteMany({ publication: publication._id }).exec();
+            await BookMark.deleteMany({ publication: publication._id }).exec();
+            await Publication.findByIdAndDelete(publication._id).exec();
+        }));
         await User.findByIdAndDelete(payload.user._id).exec();
         return res.json({ message: "User deleted", user });
     } catch (error) {
@@ -149,4 +177,56 @@ const getUser = async (req: AuthRequest, res: Response) => {
             .json({ message: "Internal server error", error: error });
     }
 };
-export { createUser, loginUser, EditUser, deleteUser, getUser, changePassword };
+
+const getAnotherUser = async (req: AuthRequest, res: Response) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    const payload: any = decryptToken(token);
+    const id = payload.user._id;
+    const { userId } = req.params;
+    try {
+        let statusUser: string = '';
+        const user: any = await User.findById(userId).exec();
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const status = await Friend.findOne({ mySelf: id, myFriend: userId }).exec();
+        if (status) {
+            statusUser = "friend";
+        } else {
+            const request = await FriendRequest.findOne({ sender: id, receiver: userId }).exec();
+            if (request) {
+                statusUser = "requestSent";
+            } else {
+                const request2 = await FriendRequest.findOne({ sender: userId, receiver: id }).exec();
+                if (request2) {
+                    statusUser = "requestReceived";
+                } else {
+                    statusUser = "noFriend";
+                }
+            }
+        }
+
+        return res.json({ message: "User found", user, status: statusUser });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal server error", error: error });
+    }
+}
+const getAllUsers = async (req: AuthRequest, res: Response) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    const payload: any = decryptToken(token);
+    const id = payload.user._id;
+    try {
+        const users = await User.find().exec();
+        if (!users) {
+            return res.status(404).json({ message: "Users not found" });
+        }
+        return res.json({ message: "Users found", users });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal server error", error: error });
+    }
+}
+export { createUser, loginUser, EditUser, deleteUser, getUser, changePassword, getAnotherUser, getAllUsers };
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -6,6 +6,8 @@ import {
     deleteUser,
     changePassword,
     getUser,
+    getAnotherUser,
+    getAllUsers
 } from "../controllers/users.controllers";
 import passport from "passport";
 
@@ -13,7 +15,7 @@ const router = express.Router();
 
 router.post("/create", createUser);
 router.post("/login", loginUser);
-
+router.get("/getAll", passport.authenticate("jwt", { session: false }), getAllUsers);
 router.put("/edit", passport.authenticate("jwt", { session: false }), EditUser);
 router.delete(
     "/delete",
@@ -25,5 +27,8 @@ router.get(
     passport.authenticate("jwt", { session: false }),
     getUser
 );
+
+router.get("/getUser/:userId", getAnotherUser);
 router.patch("/changePassword", passport.authenticate("jwt", { session: false }), changePassword);
+
 export default router;
